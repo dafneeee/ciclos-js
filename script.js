743537089c2ae25248145b837a9b96a6cac688c9
Dafne <dafnecitagodoy@gmail.com>
2020-08-25 15:37:09 -0400

    ciclos-js p1

---
+++ script.js
@@ -0,0 +1,12 @@
+
+//p1
+let inputNumero = prompt("Ingrese un numero entre 1 a 100");
+if( inputNumero > 101 && inputNumero < 0 ){
+    alert("Ingresa un numero válido")
+} else{
+    for(var i = 0; i <= inputNumero; i++){
+         console.log(i);
+    }
+}
+
+//p2
